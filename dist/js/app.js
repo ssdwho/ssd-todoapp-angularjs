597091fae394133f6f96c837328c9b1563b0c091
Ali Barın <ali.barin53@gmail.com>
2016-10-01 17:44:29 +0300

    Update some functions

--- dist/js/app.js
+++ dist/js/app.js
@@ -16,49 +16,28 @@
 		.controller('control', control)
 
 	function control($scope, $location){
-		var todosJson = JSON.parse(localStorage.getItem("todoList")) || [];
-
-		$scope.todos = todosJson;
+		$scope.todos = JSON.parse(localStorage.getItem("todoList")) || [];
 
 		$scope.$watch('todos', function(newValue, oldValue) {
 			if (angular.isObject(newValue)) {
 				localStorage.setItem('todoList', JSON.stringify(newValue));
+				$scope.toggleAll = !!$scope.countStatus();
 			}
 		}, true);
-
-		$scope.path = $location.path().split("/")[1] || "all";
 
 		/** Add Todo */
 		$scope.add = function(){
 			$scope.todos.push({ "subject": $scope.subject, "status": false });
-		}
-
-		/** Todo Update */
-		$scope.update = function(){
-			if ($scope.countStatus() == 0) {
-				$scope.toggleAll = true;
-			} else {
-				$scope.toggleAll = false;
-			}
+			$scope.subject = '';
 		}
 
 		/** Counter */
-		$scope.countStatus = function(){
-			var count = 0;
-			angular.forEach($scope.todos, function(todo){
-				count += todo.status ? 0 : 1;
-			});
-		return count;
-		}
-
-		if ($scope.countStatus() == 0 && $scope.todos != 0) {
-			$scope.toggleAll = true;
-		} else if ($scope.todos == 0) {
-			$scope.toggleAll = false;
+		$scope.countStatus = function() {
+			return $scope.todos.filter(function(todo) { return todo.status == false; }).length;
 		}
 
 		/** Checking */
-		$scope.checking = function(){
+		$scope.checking = function() {
 			$scope.todos = $scope.todos.map(function(todo) {
 				todo.status = $scope.toggleAll;
 
@@ -67,27 +46,15 @@
 		}
 
 		/** Remove One Todo */
-		$scope.removeOne = function(a) {
-			$scope.todos.splice(a, 1);
-			if ($scope.countStatus() == 0 && $scope.todos != 0) {
-				$scope.toggleAll = true;
-			} else if ($scope.todos == 0) {
-				$scope.toggleAll = false;
-			} else {
-				$scope.toggleAll = false;
-			}
+		$scope.removeOne = function(index) {
+			$scope.todos.splice(index, 1);
 		}
 
-		/** Remove All Todos */
-		$scope.removeAll = function() {
-			var allTodos = $scope.todos;
-			$scope.todos = [];
-			angular.forEach(allTodos, function(todo){
-				if (!todo.status){
-					$scope.todos.push(todo);
-				}
+		/** Remove Done Todos */
+		$scope.removeDoneTodos = function() {
+			$scope.todos = $scope.todos.filter(function(todo) {
+				return todo.status === false;
 			});
-			$scope.toggleAll = false;
 		}
 	}
 }());
